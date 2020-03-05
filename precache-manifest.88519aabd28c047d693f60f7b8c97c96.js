79f4869b2cd5c4506926ee3e9db1a74b1f58c4c5
纪飞 <fei.jay@etank.cn>
2020-03-05 09:23:04 +0800

    PWA first

---
+++ precache-manifest.88519aabd28c047d693f60f7b8c97c96.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b573f16c55bf25cf19171cc6d79bf2fa",
+    "url": "___sw.js"
+  },
+  {
+    "revision": "cd3e608839cc4c12a7b4",
+    "url": "css/app.f32fcbc9.css"
+  },
+  {
+    "revision": "cee7992561522bcf76a3f42b81cfbf3b",
+    "url": "icon.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "55c94f4105b6297496512d7761a47982",
+    "url": "index.html"
+  },
+  {
+    "revision": "e1c38d8ef5be11567782",
+    "url": "js/about.132e27fe.js"
+  },
+  {
+    "revision": "cd3e608839cc4c12a7b4",
+    "url": "js/app.6c0f9362.js"
+  },
+  {
+    "revision": "bf516036f5ca0e4cb4cd",
+    "url": "js/chunk-vendors.c769e561.js"
+  },
+  {
+    "revision": "a2ad0878a509e62ea31ae8e12927a651",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
